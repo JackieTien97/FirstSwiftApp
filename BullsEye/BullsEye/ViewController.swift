0bdcbdc9d0097cff53f082d4ef56df5f74a5fb32
Slow_Time <1395881075@qq.com>
2017-07-29 12:51:46 +0800

    单页面构建基本完成

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var labelOfTargetValue: UILabel!
+    @IBOutlet weak var labelOfRound: UILabel!
+    @IBOutlet weak var labelOfScore: UILabel!
+    
+    let initialValueOfSlider = 50
+    var currentValue: Int!
+    var targetValue: Int!
+    var round = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,11 +33,60 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "你好，刘琦", message: "你是笨蛋", preferredStyle: .alert)
-        let action = UIAlertAction(title: "我喜欢田原", style: .default, handler: nil)
+        let difference = abs(targetValue - currentValue)
+        let point = 100 - abs(targetValue - currentValue)
+        let message = "笨蛋，你的得分是：\(point)\n你的结果是：\(Int(currentValue))\n目标数值是：\(Int(targetValue))\n差值是：\(difference)"
+        score += point
+        var title: String
+        if difference == 0 {
+            title = "完美！你可以去买彩票了！"
+        }
+        else if difference < 5 {
+            title = "这运气可以中二等奖了"
+        }
+        else if difference < 10 {
+            title = "貌似还不错"
+        }
+        else {
+            title = "就你这水平，还来玩？"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "我喜欢田原", style: .default, handler: {
+            action in
+                self.startNewRound()
+        })
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
 
+    @IBAction func silderMoved(slider: UISlider) {
+//        print("滑动条的当前数值是：\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startNewGame() {
+        round = 0
+        score = 0
+        labelOfRound.text = String(round)
+        labelOfScore.text = String(score)
+        targetValue = Int(arc4random_uniform(100)) + 1
+        updateLabelOfTargetValue(targetValue: targetValue)
+        currentValue = initialValueOfSlider
+        slider.value = Float(initialValueOfSlider)
+    }
+        
+    private func startNewRound() {
+        round += 1
+        labelOfRound.text = String(round)
+        labelOfScore.text = String(score)
+        targetValue = Int(arc4random_uniform(100)) + 1
+        updateLabelOfTargetValue(targetValue: targetValue)
+        currentValue = initialValueOfSlider
+        slider.value = Float(initialValueOfSlider)
+    }
+    
+    private func updateLabelOfTargetValue(targetValue: Int) {
+        labelOfTargetValue.text = String(targetValue)
+    }
 }
 
